3fff33942103b78403d43698f27a2bd2e1cce151
bepisilek <kaposi.marci@gmail.com>
2025-11-08 15:53:10 +0100

    Enable offline-ready PWA support

---
+++ service-worker.js
@@ -0,0 +1,36 @@
+const CACHE_NAME = "munkaora-v1";
+const URLS_TO_CACHE = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(URLS_TO_CACHE))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== "GET") return;
+  event.respondWith(
+    caches.match(event.request).then((response) =>
+      response ||
+      fetch(event.request).then((fetched) => {
+        const copy = fetched.clone();
+        caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy));
+        return fetched;
+      }).catch(() => caches.match("./index.html"))
+    )
+  );
+});
