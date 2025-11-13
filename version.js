5504f60a1c5b852ede44271fd76e21bb23fb26a8
bepisilek <kaposi.marci@gmail.com>
2025-11-13 20:26:26 +0100

    Create version.js

---
+++ version.js
@@ -0,0 +1,6 @@
+// ============================================
+// MUNKAÓRA PRO - KÖZPONTI VERZIÓ
+// ============================================
+// Csak ezt a fájlt kell módosítani verzióváltáskor!
+
+const APP_VERSION = '2.5.0';
