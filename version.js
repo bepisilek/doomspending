dc0e725845780414d0caf1dc9bcd5984f27127a1
bepisilek <kaposi.marci@gmail.com>
2025-11-17 19:19:32 +0100

    Update version.js

--- version.js
+++ version.js
@@ -4,5 +4,5 @@
 // Ez az EGYETLEN hely ahol a verziót frissíteni kell!
 // BUILD TIMESTAMP: 2024-11-16 20:00:00
 
-const APP_VERSION = '7.0.0';
+const APP_VERSION = '8.0.0';
 const BUILD_TIMESTAMP = Date.now(); // Auto-generate
