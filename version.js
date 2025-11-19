a6619cd985b2e0d7aed1e0a9df31299db076e47d
bepisilek <kaposi.marci@gmail.com>
2025-11-19 12:38:04 +0100

    Update version.js

--- version.js
+++ version.js
@@ -3,9 +3,8 @@
 // ============================================
 // Ez az EGYETLEN hely ahol a verziót frissíteni kell!
 // Frissítsd a verziószámot ÉS a BUILD_TIMESTAMP-et minden éles build előtt!
-// BUILD TIMESTAMP (UTC): 2024-11-19 12:30:00
+// BUILD TIMESTAMP (UTC): 2025-11-19 11:35:15
 
-const APP_VERSION = '8.9.0';
+const APP_VERSION = '9.0.0';
 // Fontos: Ezt build időben kell rögzíteni! 
-// Ha nem buildelünk, hagyjuk így, de tudd: a Date.now() minden SW töltéskor változik!
-const BUILD_TIMESTAMP = 1700406600000; // Manuálisan frissítve, az '2024-11-19 12:30:00' UTC ideje
+const BUILD_TIMESTAMP = 1731993315000; // 2025-11-19 11:35:15 CET
